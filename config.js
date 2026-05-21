4f04303b319dde72edfaa27be9f5980df2855e0c
yaronaboo <yaron.ab@gmail.com>
2026-05-21 16:41:55 +0300

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+export const firebaseConfig = {
+          apiKey: "AIzaSyArVAPqwzW10GfRHf5M16sjCaV5qb8_Zvg",
+          authDomain: "alfa-pub-shifts.firebaseapp.com",
+          projectId: "alfa-pub-shifts",
+          storageBucket: "alfa-pub-shifts.firebasestorage.app",
+          messagingSenderId: "667070974017",
+          appId: "1:667070974017:web:22c1d591f47da567e65ec0",
+          measurementId: "G-4V9S7411BV"
+};
